a5c7f3a8ff1ad4316f5ffbe9a42fea61a1193989
Wujek <mjwojcik94@gmail.com>
2017-02-12 11:05:14 +0100

    jakis szajs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
-function ajax( options ) {
+function ajax( ajaxOptions ) {
+   var options = {
+         type: ajaxOptions.type || "POST",
+         url: ajaxOptions.url || "",
+         onComplete: ajaxOptions.onComplete || function () {},
+         onError: ajaxOptions.onError || function () {},
+         onSuccess: ajaxOptions.onSuccess || function () {},
+         dataType: ajaxOptions.dataType || "text"
+   };
    
+};
+
+function pobierzDane() {
+   
+}
+
+//utworzenie obiektu
+var httpReq = new XHTMLHttpRequest();
+
+//otwarcie połączenia 
+httpReq.open(options.type, options.url, true);
+
+//gdy status nr4 - gotowe dane
+httpReq.onreadystatechange = function () {
+   if(httpReq.readyState == 4){
+      
+   }
 }
