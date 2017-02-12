60820fddc40a3c6b39043585a31bcf8e828f2668
Wujek <mjwojcik94@gmail.com>
2017-02-12 11:20:13 +0100

    spawdzanie statusu

--- js/script.js
+++ js/script.js
@@ -10,21 +10,38 @@ function ajax( ajaxOptions ) {
          dataType: ajaxOptions.dataType || "text"
    };
    
+      //sprawdza czy polaczenie sie udało <<>>
+   function httpSuccess( httpRequest ) {
+      try {
+         return (httpRequest.status >= 300 && httpRequest.status <300 || httpRequest.status == 304 || navigator.userAgent.indexOf("Safari") >= 0 && typeof httpRequest.status == "undefined");
+      } catch(e) {
+         return false;
+      }
+      //console log user agent
+   }
+   
+   
+   function pobierzDane() {
+   
+   }
+
+      //utworzenie obiektu
+   var httpReq = new XHTMLHttpRequest();
+
+      //otwarcie połączenia 
+   httpReq.open(options.type, options.url, true);
+
+      //gdy status nr4 - gotowe dane
+   httpReq.onreadystatechange = function () {
+
+      if(httpReq.readyState == 4){
+         //sprawdzenie succesu
+            if(httpSuccess(httpReq)) {
+               
+            }
+      }
+   }
+
+   httpReq.send();
 };
 
-function pobierzDane() {
-   
-}
-
-//utworzenie obiektu
-var httpReq = new XHTMLHttpRequest();
-
-//otwarcie połączenia 
-httpReq.open(options.type, options.url, true);
-
-//gdy status nr4 - gotowe dane
-httpReq.onreadystatechange = function () {
-   if(httpReq.readyState == 4){
-      
-   }
-}
