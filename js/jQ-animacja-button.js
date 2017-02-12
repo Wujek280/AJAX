0294342946682cf674b036d6474a2c698e9c1f95
Wujek <mjwojcik94@gmail.com>
2017-02-12 16:24:28 +0100

    square

--- js/jQ-animacja-button.js
+++ js/jQ-animacja-button.js
@@ -1,14 +1,27 @@
 'use strict';
 var button = document.createElement('input');
-button.setAttribute('type', 'button');
-button.setAttribute('class', 'button');
-button.setAttribute('value', 'DAWAJ ANIMACJE');
-document.body.appendChild(button);
+button.setAttribute('id', 'przycisk');
+ button.setAttribute('type', 'button');
+ button.setAttribute('class', 'button');
+ button.setAttribute('value', 'DAWAJ ANIMACJE');
+ document.body.appendChild(button);
 
 var sq = document.createElement('div');
-sq.setAttribute('class', 'square');
-document.body.appendChild(sq);
+sq.setAttribute('id', 'sq');
+ sq.setAttribute('class', 'square');
+ //$('#sq').css.position = 'absolute';
+ document.body.appendChild(sq);
 
 $(document).ready(function () {
+   
+   $('#przycisk').click(function() {
+      
+   console.log("heh");
+      
+     $( '#sq').animate({ width:"100px", height:"100px", marginLeft:"100px" }, 3000,function() {
+        
+     });
+      
+   });
    
 });
